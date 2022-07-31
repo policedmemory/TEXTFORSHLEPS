2eb89269094843bc348b795b1b94426c881756e8
policedmemory <110331826+policedmemory@users.noreply.github.com>
2022-07-31 12:07:21 +0200

    Update and rename readme.txt to text.py

--- readme.txt
+++
@@ -1,6 +0,0 @@
-Programm by policedmemory 
-For installation: 
-git clone https://github.com/policedmemory/TEXTFORSHLEPS 
-cd TEXTFORSHLEPS 
-pip install -r requirements.txt 
-python3 text.py 
---
+++ text.py
@@ -0,0 +1,9 @@
+from pyfiglet import figlet_format
+
+teext=input("Введите текст: ")
+
+print(figlet_format(teext, font = "standard" )) 
+print("By policedmemory")
+
+
+#Программа создана policedmemory
